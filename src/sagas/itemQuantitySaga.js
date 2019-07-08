cc35e78f83ceadb980af351051bcdd944687acd7
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-08 16:20:48 +0300

    handleIncreaseItemQuantity function

--- src/sagas/itemQuantitySaga.js
+++ src/sagas/itemQuantitySaga.js
@@ -13,3 +13,15 @@ import {
 import {
     currentUserSelector
 } from '../selectors';
+
+export function* handleIncreaseItemQuantity({id}) {
+    yield put(setItemQuantityFetchStatus(FETCHING));
+    const user = yield select(currentUserSelector);
+    const response = yield call(fetch,`http://localhost:8081/cart/add/${user.get('id')}/${id}`);
+
+    if (response.status !== 200) {
+        yield put(decreaseItemQuantity(id, true));
+        alert("Sorry, there weren't enough items in stock to complete your request.");
+    }
+    yield put(setItemQuantityFetchStatus(FETCHED));
+}
