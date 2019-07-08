5e079eaca457d18eb980d468a0349babe1f08e13
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-08 16:21:35 +0300

    itemQuantitySaga

--- src/sagas/itemQuantitySaga.js
+++ src/sagas/itemQuantitySaga.js
@@ -25,3 +25,23 @@ export function* handleIncreaseItemQuantity({id}) {
     }
     yield put(setItemQuantityFetchStatus(FETCHED));
 }
+
+export function* handleDecreaseItemQuantity({id, local}) {
+    if (local) {
+        return;
+    }
+    yield put(setItemQuantityFetchStatus(FETCHING));
+    const user = yield select(currentUserSelector);
+    const response = yield call(fetch,`http://localhost:8081/cart/remove/${user.get('id')}/${id}`);
+    if (response.status !== 200) {
+        console.warn("Received non-200 status:: ", response);
+    }
+    yield put(setItemQuantityFetchStatus(FETCHED));
+}
+
+export function* itemQuantitySaga() {
+    yield [
+        takeLatest(DECREASE_ITEM_QUANTITY, handleDecreaseItemQuantity),
+        takeLatest(INCREASE_ITEM_QUANTITY, handleIncreaseItemQuantity)
+    ]
+}
