aa008e76e01a4548f1c80afac92680683b7d45ba
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-08 15:57:20 +0300

    Load item details

---
+++ src/sagas/itemDetailsSaga.js
@@ -0,0 +1,15 @@
+import { take, fork, put } from 'redux-saga/effects'
+import fetch from 'isomorphic-fetch';
+
+import {
+    SET_CART_ITEMS,
+    setItemDetails
+} from './../actions';
+
+export function* loadItemDetails(item){
+    const { id } = item;
+    const response = yield fetch(`http://localhost:8081/items/${id}`)
+    const data = yield response.json();
+    const info = data[0];
+    yield put(setItemDetails(info));
+}
